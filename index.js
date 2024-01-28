f95138a1c13105ca47c6391f9fd4a6864a5c7432
sadid369 <sadid.jones@gmail.com>
2024-01-28 21:41:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+// !important! 
+// you need to install the following libraries |express|[dotenv > if required]
+// or run this command >> npm i express dotenv 
+
+app.get('/' , (req , res)=>{
+
+   res.json({"name":'sadid'})
+
+})
+
+
+
+app.listen(port , ()=> console.log('> Server is up and running on port : ' + port))
