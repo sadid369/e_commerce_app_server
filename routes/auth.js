26eb078a89b4d4bdd86c6336683349a9c10129f3
sadid369 <sadid.jones@gmail.com>
2024-01-30 22:31:19 +0600

    start sign in functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
-
 const authRouter = require('express').Router()
 const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 
 authRouter.post('/api/signup', async (req, res) => {
   const { name, email, password } = req.body;
+  console.log(name, email, password);
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
@@ -26,6 +27,27 @@ authRouter.post('/api/signup', async (req, res) => {
   } catch (e) {
     res.status(500).json({ error: e.message })
   }
+
+})
+
+authRouter.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email })
+    if (!user) {
+      return res.status(400).json({ msg: "User does not exists" })
+    }
+    const isPasswordMatch = await bcryptjs.compare(password, user.password)
+    if (!isPasswordMatch) {
+      return res.status(400).json({ msg: "Password not Match" })
+    }
+    const token = jwt.sign({ id: user._id }, process.env.KEY1)
+    res.json({ token, ...user._doc })
+  } catch (e) {
+    res.status(500).json({ error: e.message })
+  }
+
 
 })
 
