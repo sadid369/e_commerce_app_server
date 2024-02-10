d09c27a3f343adb24190c249aa0200d8cdd0720a
sadid369 <sadid.jones@gmail.com>
2024-02-10 17:30:06 +0600

    6:09  delete product start

--- models/product.js
+++ models/product.js
@@ -28,7 +28,7 @@ const productSchema = new mongoose.Schema(
             required: true
         },
         price: {
-            type: String,
+            type: Number,
             required: true
         },
 
--- routes/admin.js
+++ routes/admin.js
@@ -19,5 +19,25 @@ adminRoute.post('/admin/add-product', admin, (req, res) => {
 
 })
 
+adminRoute.get('/admin/get-product', admin, async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.json(products)
+    } catch (error) {
+        res.status(500).json({ error: e.message })
+    }
+})
+adminRoute.post('/admin/delete-product', admin, async (req, res) => {
+    const { id } = req.body
+    try {
+        await Product.findByIdAndDelete({ _id: id })
+        res.json({ msg: 'Product Delete Successfully' })
+    } catch (e) {
+        res.status(500).json({ error: e.message })
+    }
+})
+
+
+
 
 module.exports = adminRoute
