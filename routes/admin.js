852e61fda4bffaa8c058047a4d49e6ff0b0699bc
sadid369 <sadid.jones@gmail.com>
2024-02-19 23:27:56 +0600

    10:45  completed order delivery traker in admin side

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const admin = require('../middlewares/admin');
 const { Product } = require('../models/product');
+const Order = require('../models/order');
 
 const adminRoute = express.Router()
 
@@ -37,7 +38,25 @@ adminRoute.post('/admin/delete-product', admin, async (req, res) => {
         res.status(500).json({ error: error.message })
     }
 })
-
+adminRoute.get('/admin/get-orders', admin, async (req, res) => {
+    try {
+        const orders = await Order.find({})
+        res.json(orders)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+adminRoute.post('/admin/change-order-status', admin, async (req, res) => {
+    const { id, status } = req.body
+    try {
+        let order = await Order.findById(id)
+        order.status = status
+        order = await order.save()
+        res.json(order)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
 
 
 
