674a6c47fad3c2216ec4c2edf5b7c11a355807a0
sadid369 <sadid.jones@gmail.com>
2024-02-21 17:09:39 +0600

    finished

--- routes/admin.js
+++ routes/admin.js
@@ -57,7 +57,50 @@ adminRoute.post('/admin/change-order-status', admin, async (req, res) => {
         res.status(500).json({ error: error.message })
     }
 })
+adminRoute.get('/admin/analytics', admin, async (req, res) => {
+    try {
+        const orders = await Order.find({});
+        let totalEarnings = 0;
+        for (let i = 0; i < orders.length; i++) {
+            for (let j = 0; j < orders[i].products.length; j++) {
+                totalEarnings += orders[i].products[j].quantity * orders[i].products[j].product.price
 
+            }
 
+        }
+        let mobileEarnings = await fetchCategoryWiseProduct('Mobiles')
+        let essentialsEarnings = await fetchCategoryWiseProduct('Essentials')
+        let appliancesEarnings = await fetchCategoryWiseProduct('Appliances')
+        let booksEarnings = await fetchCategoryWiseProduct('Books')
+        let fashionEarnings = await fetchCategoryWiseProduct('Fashion')
+        let earnings = {
+            totalEarnings,
+            mobileEarnings,
+            essentialsEarnings,
+            appliancesEarnings,
+            booksEarnings,
+            fashionEarnings,
+
+        }
+        res.json(earnings)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
+async function fetchCategoryWiseProduct(category) {
+    let earnings = 0;
+    let categoryOrders = await Order.find({
+        'products.product.category': category
+    })
+    for (let i = 0; i < categoryOrders.length; i++) {
+        for (let j = 0; j < categoryOrders[i].products.length; j++) {
+            earnings += categoryOrders[i].products[j].quantity * categoryOrders[i].products[j].product.price
+
+        }
+
+    }
+    return earnings;
+}
 
 module.exports = adminRoute
